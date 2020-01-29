45c9c76dd0addffafdd80da86d262aaa42e1e8fd
sa-bt <bakhshian2020@gmail.com>
2020-01-30 03:10:36 +0330

    Updates

---
+++ precache-manifest.2fd4d66cdd60826a53e2f6dc52b1dfbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab61095a7480b2b02565b502d2ee0338",
+    "url": "/resome/index.html"
+  },
+  {
+    "revision": "185b858f215167b6b120",
+    "url": "/resome/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7ee57dca49decd6fde40",
+    "url": "/resome/static/css/main.fbd2215b.chunk.css"
+  },
+  {
+    "revision": "185b858f215167b6b120",
+    "url": "/resome/static/js/2.4525fe14.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/resome/static/js/2.4525fe14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ee57dca49decd6fde40",
+    "url": "/resome/static/js/main.1f5845c9.chunk.js"
+  },
+  {
+    "revision": "3f517217856d3fb238c4",
+    "url": "/resome/static/js/runtime-main.9739b8f2.js"
+  }
+]);
